6838bc53800c176bb4120bba0f740c70285fcf46
wWcheburekWw <gapdanil@yandex.ru>
2024-02-26 20:56:07 +0300

    Services added

---
+++ FileParser/Services/InstrumentStatusService.cs
@@ -0,0 +1,48 @@
+﻿using FileParser.Entities;
+using FileParser.Interfaces;
+using System.Text.Json;
+
+namespace FileParser.Services;
+
+public class InstrumentStatusService : IStatusService
+{
+    private readonly IStatusReader _statusReader;
+    private readonly IMessageBroker _messageBroker;
+
+    public InstrumentStatusService(IStatusReader statusReader, IMessageBroker messageBroker)
+    {
+        _statusReader = statusReader;
+        _messageBroker = messageBroker;
+    }
+
+    public InstrumentStatus ChangeModuleState(ref InstrumentStatus instrumentStat)
+    {
+        foreach(DeviceStatus deviceStat in instrumentStat.DeviceStatuses)
+        {
+            deviceStat.RapidControlStatus.ModuleState = new Random().Next(4) switch
+            {
+                0 => "Online",
+                1 => "Run",
+                2 => "NotReady",
+                3 => "Offline"
+            };
+        }
+
+        return instrumentStat;
+    }
+
+    public void PublishInstrumentStatus(InstrumentStatus instrumentStatus)
+    {
+        var json = JsonSerializer.Serialize(instrumentStatus, new JsonSerializerOptions()
+        {
+            WriteIndented = true
+        });
+        Console.WriteLine(json);
+        _messageBroker.Publish(json);
+    }
+
+    public async Task<InstrumentStatus> ReadInstrumentStatusAsync()
+    {
+        return await _statusReader.ReadAsync();
+    }
+}
---
+++ FileParser/Services/MessageProducer.cs
@@ -0,0 +1,39 @@
+﻿using FileParser.Interfaces;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace FileParser.Services;
+
+public class MessageProducer : IMessageBroker
+{
+    private readonly IModel channel;
+    private readonly IConnection connection;
+    private readonly string queueKey;
+    public ProducerQueueOptions QueueOptions { get; set; }
+    public IBasicProperties? PublishProperties { get; set; }
+
+    public MessageProducer(string queueKey, string hostName = "localhost")
+    {
+        QueueOptions = new ProducerQueueOptions();
+        PublishProperties = null;
+
+        var factory = new ConnectionFactory() { HostName = hostName };
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        channel.QueueDeclare(queueKey, QueueOptions.durable, QueueOptions.exclusive, QueueOptions.autoDelete, QueueOptions.arguments);
+
+        this.queueKey = queueKey;
+    }
+
+    public void Publish(string json)
+    {
+        var body = Encoding.UTF8.GetBytes(json);
+        channel.BasicPublish(string.Empty, queueKey, PublishProperties, body);
+    }
+
+    ~ MessageProducer()
+    {
+        channel.Dispose();
+        connection.Dispose();
+    }
+}
