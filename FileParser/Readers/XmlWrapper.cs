b6fb906072c8882d238afdf7d749ce7088856a87
wWcheburekWw <gapdanil@yandex.ru>
2024-02-26 20:53:35 +0300

    XmlReader namespace updated

---
+++ FileParser/Readers/XmlWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml;
+
+namespace FileParser.Readers;
+
+public static class XmlWrapper
+{
+    public static XmlNode SelectSingle(XmlNode context, string xPath)
+    {
+        Console.WriteLine($"[*] Trying to read {xPath} from {context.Name}");
+        var result = context.SelectSingleNode(xPath);
+        if(result == null)
+        {
+            throw new Exception();
+        }
+        Console.WriteLine("[V] Success\n");
+        return result;
+    }
+
+    public static XmlNodeList SelectAny(XmlNode context, string xPath)
+    {
+        Console.WriteLine($"[*] Trying to read {xPath} from {context.Name}");
+        var result = context.SelectNodes(xPath);
+        if(result == null)
+        {
+            throw new Exception();
+        }
+        Console.WriteLine("[V] Success\n");
+        return result;
+    }
+}
