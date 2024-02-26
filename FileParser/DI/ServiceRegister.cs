fb93a5608d7eb4478746db87579bae2fc3add6b6
wWcheburekWw <gapdanil@yandex.ru>
2024-02-26 20:57:37 +0300

    DI extension created

---
+++ FileParser/DI/ServiceRegister.cs
@@ -0,0 +1,16 @@
+﻿using FileParser.Interfaces;
+using FileParser.Readers;
+using FileParser.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace FileParser.DI;
+public static class ServiceRegister
+{
+    public static void AddContracts(this IServiceCollection services, string producerQueueKey)
+    {
+        services.AddScoped<IStatusReader, InstrumentStatusReader>((provider) => new InstrumentStatusReader("../../../Source/status.xml"));
+        services.AddScoped<IStatusService, InstrumentStatusService>();
+        services.AddScoped<IMessageBroker, MessageProducer>((provider) => new MessageProducer(producerQueueKey));
+    }
+}
