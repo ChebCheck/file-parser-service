ed88da13269afd3daa2c6a2d4e02ef712016d331
wWcheburekWw <gapdanil@yandex.ru>
2024-02-27 06:27:04 +0300

    Program.cs added

---
+++ FileParser/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FileParser.DI;
+using FileParser.Interfaces;
+
+ServiceCollection services = new ServiceCollection();
+services.AddServices("InstrumentStatus");
+
+var serviceProvider = services.BuildServiceProvider();
+var instrumentService = serviceProvider.GetRequiredService<IStatusService>();
+
+CancellationToken token = new CancellationToken();
+Task task = Task.Run(async () =>
+{
+    while (true)
+    {
+        var instrumentStatus = await instrumentService.ReadInstrumentStatusAsync();
+        instrumentService.ChangeModuleState(ref instrumentStatus);
+        instrumentService.PublishInstrumentStatus(instrumentStatus);
+        Thread.Sleep(1000);
+    }
+}, token);
+
+Console.WriteLine("Pres any key to exit");
+Console.ReadKey();
+token = new CancellationToken(true);
